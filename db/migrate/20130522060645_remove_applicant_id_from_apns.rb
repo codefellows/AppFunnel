fd7f2ed39856b079bd9f260639529bf400151ed5
Billy Shih <me@billyshih.com>
2013-05-29 16:09:25 -0700

    Removed unneeded files

--- db/migrate/20130522060645_remove_applicant_id_from_apns.rb
+++
@@ -1,9 +0,0 @@
-class RemoveApplicantIdFromApns < ActiveRecord::Migration
-  def up
-    remove_column :apns, :applicant_id
-  end
-
-  def down
-    add_column :apns, :applicant_id, :integer
-  end
-end
