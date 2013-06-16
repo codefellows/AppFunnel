d3e7fa525612e2fdf4741e6e0d3fd77de3192e2b
Billy Shih <me@billyshih.com>
2013-06-15 20:25:40 -0700

    added client_side_validation gem and added validations to profile/apn forms

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,29 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :integer          not null, primary key
+#  first_name   :string(255)
+#  last_name    :string(255)
+#  email        :string(255)
+#  phone_number :string(255)
+#  city         :string(255)
+#  state        :string(255)
+#  gender       :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :integer
+#  twitter      :string(255)
+#  linkedin     :string(255)
+#  github       :string(255)
+#
+
 class Profile < ActiveRecord::Base
   has_one :apn, autosave: true, :dependent => :delete
   accepts_nested_attributes_for :apn
+  belongs_to :user
 
   validates :email, presence: true, uniqueness: true
   validates :first_name, :last_name, :phone_number,
-    :city, :state, presence: true
-  belongs_to :user
-  
+    :city, :state, presence: true    
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id              :integer          not null, primary key
+#  user_id         :integer
+#  apn_id          :integer
+#  education       :integer
+#  contribution    :integer
+#  resume          :integer
+#  fit             :integer
+#  work_experience :integer
+#  note            :text
+#  exceptional     :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  total           :integer
+#
+
 require 'spec_helper'
 
 describe Review do
