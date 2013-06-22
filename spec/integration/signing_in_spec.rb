0bdd42290c8994ed2a3cf44a2883817af981a5d6
Billy Shih <me@billyshih.com>
2013-06-22 16:05:26 -0700

    Started work on signing in spec

--- spec/integration/signing_in_spec.rb
+++ spec/integration/signing_in_spec.rb
@@ -5,18 +5,13 @@ feature "Signing in" do
     Factory(:user, :email => "spanky@cavendish.com")
   end
 
-  scenario "Signing in via confirmation" do
-    open_email "spanky@cavendish.com", :with_subject => /Confirmation/
-    click_first_link_in_email
-    page.should have_content("Your account was successfully confirmed")
-  end
-
   scenario "Signing in via form" do
-    User.find_by_email("spanky@cavendish.com").confirm!
-    visit '/'
-    fill_in "Email", :with => "spanky@cavendish.com"
-    fill_in "Password", :with => "password"
-    click_button "Sign in"
-    page.should have_content("Signed in successfully.")
+      visit '/'
+      within "#login" do
+        fill_in "Email", :with => "spanky@cavendish.com"
+        fill_in "Password", :with => "password"
+        click_button "Sign in"
+        page.should have_content("Signed in successfully.")
+      end
   end
 end
