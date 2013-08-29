7be8f6e2b9fa0ec86d2c6265f90320922e94f942
bethanyr <bethany.rentz@hotmail.com>
2013-08-29 16:28:48 -0700

    implemented selecting decision for each class

--- app/controllers/course_registrations_controller.rb
+++ app/controllers/course_registrations_controller.rb
@@ -4,6 +4,10 @@ class CourseRegistrationsController < ApplicationController
   def update
     if @course_registration.update_attributes(course_registration_params)
       flash[ :notice] = "Registration has been updated."
+      respond_to do |format|
+        format.html { redirect_to reviews_url }
+        format.js
+      end
     else flash[ :alert] = "Registration has not been updated."
       render :action => "edit"
     end
@@ -23,7 +27,7 @@ class CourseRegistrationsController < ApplicationController
   end
 
   def course_registration_params
-    params.permit(:id, :decision)
+    params.require(:course_registration).permit(:id, :decision, :apn_id, :course_id)
   end
 
 end
